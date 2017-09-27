e7d090dc3e6694c1a822fdd5a741e5fc44f1d543
Jess Leggatt <jessieleggatt@Jesss-MBP.hitronhub.home>
2017-09-27 13:21:06 -0700

    Adding a file

---
+++ java.js
@@ -0,0 +1,12 @@
+function sortList() {
+    var txt;
+    var list = prompt("Please enter your unordered list:", "Input");
+    if (list == null || list == "") {
+      txt = "User cancelled the prompt.";
+    } else {
+      txt = list;
+    }
+  
+    var array = txt.split(',');
+    array.sort();
+    document.getElementById("jess").innerHTML = "Your ordered list: " + array;
